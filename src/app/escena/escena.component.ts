78d8672d91098e0c3abb13465a6f58001c55c9c7
deuvede24 <cdaniela.vd@gmail.com>
2024-06-10 12:49:05 +0200

    animation1 ok

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -2,13 +2,26 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { iStep } from '../iStep';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-escena',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './escena.component.html',
-  styleUrls: ['./escena.component.scss']
+  styleUrls: ['./escena.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':increment', [
+        style({ transform: 'translateX(100%)' }),
+        animate('450ms ease-out', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition(':decrement', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('450ms ease-out', style({ transform: 'translateX(0%)' }))
+      ]),
+    ])
+  ]
 })
 export class EscenaComponent {
 
